b3a6211ffbca93c324315a8b20d46012865728e6
Jonathan Cox <60948953+jonathancox1@users.noreply.github.com>
2022-05-11 20:50:00 -0400

    Time: 192 ms (24.29%), Space: 53.6 MB (47.14%) - LeetHub

---
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.ts
@@ -0,0 +1,11 @@
+function findDuplicates(nums: number[]): number[] {
+    nums.sort((a, b) => a - b);
+    
+    let ans = [];
+    
+    for (let x = 0; x < nums.length; x++) {
+        if (nums[x] === nums[x + 1]) ans.push(nums[x]);
+    }
+    
+    return ans;
+};
