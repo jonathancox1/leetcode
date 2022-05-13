017c376f806ed8ea9d8849134dfa9f9fc56a0063
Jonathan Cox <60948953+jonathancox1@users.noreply.github.com>
2022-05-12 21:59:57 -0400

    Time: 1109 ms (34.30%), Space: 125.2 MB (36.54%) - LeetHub

--- 146-lru-cache/146-lru-cache.ts
+++ 146-lru-cache/146-lru-cache.ts
@@ -8,24 +8,24 @@ class LRUCache {
     cap;
 
     get(key: number): number {
-        
         if (!this.map.has(key)) return - 1;
         
+        // retain existing value
         let value = this.map.get(key);
         
+        // remove delete key, and re-set to update insertion order
         this.map.delete(key);
-        
         this.map.set(key, value);
         
         return value;
     }
 
     put(key: number, value: number): void {
-        
+        // remove delete key, and re-set to update insertion order
         if (this.map.has(key)) this.map.delete(key);
-
         this.map.set(key, value);
-        
+
+        // remove the key => lookup by keys() insertion order, iterate, value = key value
         if (this.map.size > this.cap) this.map.delete(this.map.keys().next().value)
     }
 }
