dfc3182b0eb3f9e23bc9535888f862c216355740
Jonathan Cox <60948953+jonathancox1@users.noreply.github.com>
2022-05-14 14:32:07 -0400

    Time: 101 ms (26.67%), Space: 42.8 MB (74.94%) - LeetHub

---
+++ 198-house-robber/198-house-robber.ts
@@ -0,0 +1,16 @@
+function rob(nums: number[]): number {
+    if (nums.length <= 2) return Math.max(...nums);
+    
+    let max = Array(nums.length).fill(0);
+    
+    max[0] = nums[0];
+    max[1] = Math.max(nums[0], nums[1]);
+    
+    for (let x = 2; x < nums.length; x++) {
+        
+        max[x] = Math.max(max[x - 1], nums[x] + max[x - 2]);
+        
+    }
+    
+    return max[max.length - 1];
+};
