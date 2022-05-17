a8b2974a2f351a9f394685611d355821485749fb
Jonathan Cox <60948953+jonathancox1@users.noreply.github.com>
2022-05-16 20:43:40 -0400

    Time: 116 ms (62.96%), Space: 50.1 MB (93.00%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.ts
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.ts
@@ -11,49 +11,51 @@
  */
 
 function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
-        let lenA = 0, lenB = 0;
+//         let lenA = 0, lenB = 0;
 
+//     for (let i = headA; i != null; i = i.next) lenA++;
+//     for (let i = headB; i != null; i = i.next) lenB++;
+
+//     let diff = Math.abs(lenA - lenB);
+//     let longerList = lenA > lenB ? headA : headB;
+//     let shorterList = lenA > lenB ? headB : headA;
+
+//     for (let i = 0; i < diff; i++) longerList = longerList.next;
+
+//     while (longerList !== null || shorterList !== null) {
+//         if (longerList === shorterList) return shorterList;
+//         longerList = longerList.next;
+//         shorterList = shorterList.next;
+//     }
+
+    let lenA = 0;
+    let lenB = 0;
     for (let i = headA; i != null; i = i.next) lenA++;
     for (let i = headB; i != null; i = i.next) lenB++;
-
-    let diff = Math.abs(lenA - lenB);
-    let longerList = lenA > lenB ? headA : headB;
-    let shorterList = lenA > lenB ? headB : headA;
-
-    for (let i = 0; i < diff; i++) longerList = longerList.next;
-
-    while (longerList !== null || shorterList !== null) {
-        if (longerList === shorterList) return shorterList;
-        longerList = longerList.next;
-        shorterList = shorterList.next;
-    }
-
-    return null;
-//     let lenA = 0;
-//     for (let x = headA; x !== null; x = x.next) lenA++;
-    
-//     let lenB = 0;
-//     for (let x = headB; x !== null; x = x.next) lenB++;
 
 //     let longerList = lenA > lenB ? headA : headB;
 //     let shorterList = lenA < lenB ? headA : headB;
     
 //     let diff = Math.abs(lenA - lenB);
     
-//     // call next on longer until lengths are the same
-//     for (let x = 0; x < diff; x++) longerList = longerList.next;
+        let diff = Math.abs(lenA - lenB);
+    let longerList = lenA > lenB ? headA : headB;
+    let shorterList = lenA > lenB ? headB : headA;
     
-//     // at this position the length of the remainig nodes are equal
-//     // perform check, I will use longer to iterate, though it does not matter
+    // call next on longer until lengths are the same
+    for (let x = 0; x < diff; x++) longerList = longerList.next;
+    
+    // at this position the length of the remainig nodes are equal
+    // perform check, I will use longer to iterate, though it does not matter
 
-//     while (longerList !== null || shorterList !== null) {
+    while (longerList !== null || shorterList !== null) {
         
-//         // intersection point
-//         if (longerList === shorterList) return longerList;
+        // intersection point
+        if (longerList === shorterList) return shorterList;
         
-//         longerList = longerList.next;
-//         shorterList = shorterList.next;
-//     }
+        longerList = longerList.next;
+        shorterList = shorterList.next;
+    }
     
 //     return null;
 };
