0be12927a9f09217f06ddc19ed9d31d89866ada6
Jonathan Cox <60948953+jonathancox1@users.noreply.github.com>
2022-05-16 21:09:21 -0400

    Time: 119 ms (61.70%), Space: 50.4 MB (48.94%) - LeetHub

--- 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.ts
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.ts
@@ -1,24 +1,32 @@
 function findRestaurant(list1: string[], list2: string[]): string[] {
-    let map = new Map();
     
+    // build map for faster lookups
+    // 'shogun' => 0;
+    // 'kfc' => 1;
+    
+    let map = new Map();
     list1.forEach((str, i) => map.set(str, i));
     
     let leastSum = Infinity;
     let leastSumArr = [];
-    
+
     for (let i = 0; i < list2.length; i++) {
         let curr = list2[i]
         
         if (map.has(curr)) {
+            
+            // create the sum the index from both lists
             let idx = map.get(curr)
             let currLeastSum = idx + i;
             
+            // sums are equal, then append to our list
             if (leastSum === currLeastSum) {
                 
                 leastSum = currLeastSum;
                 leastSumArr.push(curr);
             }
             
+            // sums are not equal, over-ride existing list
             if (leastSum > currLeastSum) {
                 
                 leastSum = currLeastSum;
