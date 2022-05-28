84d19ddf396c158bdbaf4a4009f6fa34d0fa66f6
Jonathan Cox <60948953+jonathancox1@users.noreply.github.com>
2022-05-28 15:53:12 -0400

    Time: 247 ms (50.00%), Space: 59 MB (70.00%) - LeetHub

---
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.ts
@@ -0,0 +1,5 @@
+function kthLargestNumber(nums: string[], k: number): string {
+    const sorted: BigInt[] = [...nums].map((num) => BigInt(num)).sort((a, b) => a >= b ? 1 : -1);
+    
+    return sorted[nums.length - k].toString();
+};
